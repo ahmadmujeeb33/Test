ca745c1846f20f09a6b7dcd6c8d4039c9a1511b1
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2022-04-30 15:37:59 -0400

    adding number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int x = 2;
+	    int y = 3;
+
+        System.out.println("sum is " + x + y);
     }
 }
