3024b4f9807896a3d1ab4809c9f9001b3712c900
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2022-04-30 16:33:15 -0400

    zzzzzzzzzzzzzz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         int x = 1;
-        int y = 1;
+        int y = 2;
 
         System.out.println("sum is " + x + y);
     }
